ffa5af786fa00a793fa7f5c2aff70c52b0d08f84
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2024-03-14 12:49:01 +0200

    Models

---
+++ Models/Video.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace artHouse.Models
+{
+    public class Video
+    {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Preview { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Duration { get; set; }
+        public int Views { get; set; }
+        public int Likes { get; set; }
+        public string Uploader { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string[] Tags { get; set; }
+        public float[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Video>(this);
+    }
+}
