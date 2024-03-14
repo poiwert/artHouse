ca3ba270137ac2c4aa0c7e1aab3e6b5858c91b1b
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2024-03-14 12:55:05 +0200

    Add essential files

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+﻿using artHouse.Models;
+using artHouse.Services;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace artHouse
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddControllers();
+            services.AddTransient<JsonFileVideoService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                //endpoints.MapGet("/videos", (context) =>
+                //{
+                //    var videos = app.ApplicationServices.GetService<JsonFileVideosService>().GetVideos();
+                //    var json = JsonSerializer.Serialize<IEnumerable<Video>>(videos);
+                //    return (context).Response.WriteAsync(json);
+                //});
+            });
+        }
+    }
+}
