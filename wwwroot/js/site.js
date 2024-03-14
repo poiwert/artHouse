ca9f7ce185039d5ccc331dc4867bac21364eed59
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2024-03-14 12:53:31 +0200

    wwwroot

---
+++ wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.querySelector('.search-button-container');
+    const registrationButton = document.querySelector('.registration-button');
+    const searchBar = document.querySelector('.search-bar');
+    const infoButton = document.querySelector('.info-button');
+
+    searchButton.addEventListener('focus', function () {
+        this.classList.add('focused');
+    });
+
+    searchButton.addEventListener('blur', function () {
+        this.classList.remove('focused');
+    });
+
+    registrationButton.addEventListener('focus', function () {
+        this.classList.add('focused');
+        window.location.href = 'pop.up.html';
+    });
+
+    registrationButton.addEventListener('blur', function () {
+        this.classList.remove('focused');
+    });
+
+    searchBar.addEventListener('focus', function () {
+        this.classList.add('focused');
+    });
+
+    searchBar.addEventListener('blur', function () {
+        this.classList.remove('focused');
+    });
+    infoButton.addEventListener('focus', function () {
+        this.classList.add('focused');
+    });
+
+    infoButton.addEventListener('blur', function () {
+        this.classList.remove('focused');
+    });
+});
