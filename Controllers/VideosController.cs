ac61a0be6334f26c8e378aae647825d204e1ca56
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2024-03-14 12:48:19 +0200

    Controllers

---
+++ Controllers/VideosController.cs
@@ -0,0 +1,41 @@
+﻿using artHouse.Models;
+using artHouse.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace artHouse.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VideosController : ControllerBase
+    {
+
+        public VideosController(JsonFileVideoService videoService)
+        {
+
+            this.VideoService = videoService;
+
+        }
+
+        public JsonFileVideoService VideoService { get; }
+
+        [HttpGet]
+        public IEnumerable<Video> Get()
+        {
+
+            return VideoService.GetVideos();
+
+        }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery]string VideoId,
+            [FromQuery] int Rating)
+        {
+            VideoService.AddRating(VideoId, Rating);
+            return Ok();
+        }
+    }
+}
