8bbcd25963c1cf62e0b8ff94fac05a86b7a0d1a5
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2024-03-14 12:50:55 +0200

    Pages

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using artHouse.Models;
+using artHouse.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace artHouse.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileVideoService VideoService;
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileVideoService videoService)
+        {
+            _logger = logger;
+            VideoService = videoService;
+            Videos = VideoService.GetVideos(); 
+        }
+
+        public IEnumerable<Video> Videos { get; private set; }
+
+        public void OnGet()
+        {
+            Videos = VideoService.GetVideos();
+        }
+    }
+}
